0223741e5837c8c8c0f985b560860f207e7f2253
Vignesh Pai <vigneshpai27@gmail.com>
2026-08-07 11:48:13 +0530

    Fet Swagger & CORs update

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ dotenv.config();
 
 import Fastify from 'fastify'
 import mongodbPluggin from "./plugins/mongodb.js"
+import fastifyCors from "@fastify/cors";
+import swagger from '@fastify/swagger';
+import swaggerUi from '@fastify/swagger-ui';
 import productRoutes from './routes/product.js'
 import jwtPlugin from "./plugins/jwt.js";
 import userRoutes from './routes/users.js';
@@ -16,6 +19,35 @@ const fastify = Fastify({
   logger: true
 })
 
+// Register the plugin with your configuration
+await fastify.register(fastifyCors, {
+    // Configuration options go here
+    origin: '*',
+    //   origin: 'https://yourfrontend.com',
+    //   methods: ['GET', 'POST', 'PUT', 'DELETE']
+});
+
+
+// 1. Register the Swagger specification generator
+await fastify.register(swagger, {
+    openapi: {
+        info: {
+            title: 'My Fastify API',
+            description: 'Automatically generated OpenAPI documentation',
+            version: '1.0.0'
+        },
+        servers: [{ url: `http://localhost:${process.env.PORT}` }]
+    }
+});
+
+// 2. Register the UI plugin to serve the interactive dashboard
+await fastify.register(swaggerUi, {
+    routePrefix: '/documentation', // Access via http://localhost:5000/documentation
+    uiConfig: {
+        docExpansion: 'full',
+        deepLinking: false
+    }
+});
 
 // register mongodb database pluggin
 await fastify.register(mongodbPluggin);
